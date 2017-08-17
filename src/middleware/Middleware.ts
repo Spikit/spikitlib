fd3896b2efa69cc1e2aa4ff004f9956ccd74448a
Ryan <untuned20@gmail.com>
2017-08-17 11:19:40 -0500

    middleware

--- src/middleware/Middleware.ts
+++ src/middleware/Middleware.ts
@@ -1,5 +1,5 @@
-import { Response, NextFunction } from 'express'
-import { SpikitRequest } from '../interfaces'
+// import { Response, NextFunction } from 'express'
+// import { SpikitRequest } from '../interfaces'
 
 export abstract class Middleware {
   // public abstract handle(req: SpikitRequest, res: Response, next: NextFunction);
